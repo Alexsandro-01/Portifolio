8af018baf9b8c09571d635dedfe096bf852ec62a
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-01-13 11:24:11 -0300

    refactor: :recycle: Passa o Header para um componente

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,42 +15,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-    <div className="wrapper">
-      <div className="logo">
-        <h1>A</h1>
-      </div>
-      <nav className="wide">
-        <div>
-          {/* <!-- <button>X</button> --> */}
-          <ul>
-            <li>
-              <a href="#home">Início</a>
-            </li>
-            <li>
-              <a href="#about-me">About</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <nav className="mobile">
-        <button>X</button>
-        <div className='toogle-container'>
-          <div className="blur"></div>
-          <ul>
-            <li>
-              <a href="#home">Início</a>
-            </li>
-            <li>
-              <a href="#about-me">About</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-  </header>
+      <Header />
       <main className={styles.main}>
-       
+
       </main>
     </>
   )
