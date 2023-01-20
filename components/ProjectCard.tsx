91e2cc371f08eebbd1448898af9815ee0a94a7af
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-01-20 19:59:50 -0300

    refactor: :recycle: Estrutura par afacilitar estilização

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import { TGithubResponse } from '../types/TGithubResponse'
 import styles from '../styles/Cards.module.css';
-import {FaGithub, FaExternalLinkAlt} from 'react-icons/fa';
+import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 
-function ProjectCard({repo}: { repo: TGithubResponse }) {
+function ProjectCard({ repo }: { repo: TGithubResponse }) {
   return (
     <div className={styles.card}>
       <div>
@@ -13,26 +13,30 @@ function ProjectCard({repo}: { repo: TGithubResponse }) {
         </picture>
       </div>
       <div className={styles.description}>
-        <h3>{repo.name}</h3>
-        <p>{repo.description}</p>
-        <ul>
-          {
-            repo.topics.map((topic) => (
-              <li key={topic}>{topic}</li>
-            ))
-          }
-        </ul>
-        <div className={styles.links}>
-          <a href={repo.html_url} target="_blank" rel="noreferrer">
-            <FaGithub />
-          </a>
-          {
-            repo.homepage && (
-              <a href={repo.homepage} target="_blank" rel="noreferrer">
-                <FaExternalLinkAlt />
-              </a>
-            )
-          }
+        <article>
+          <h3>{repo.name}</h3>
+          <p>{repo.description}</p>
+        </article>
+        <div>
+          <ul>
+            {
+              repo.topics.map((topic) => (
+                <li key={topic}>{topic}</li>
+              ))
+            }
+          </ul>
+          <div className={styles.links}>
+            <a href={repo.html_url} target="_blank" rel="noreferrer">
+              <FaGithub />
+            </a>
+            {
+              repo.homepage && (
+                <a href={repo.homepage} target="_blank" rel="noreferrer">
+                  <FaExternalLinkAlt />
+                </a>
+              )
+            }
+          </div>
         </div>
       </div>
     </div>
