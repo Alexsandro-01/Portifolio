14a3acf21395cdb55d31e82dda0796879a82a52e
Alexsandro Pinheiro Ferreira <73038442+Alexsandro-01@users.noreply.github.com>
2023-01-30 18:49:36 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,5 +5,5 @@ import { Fira_Code } from '@next/font/google'
 const firaCode = Fira_Code({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component classaName={firaCode.className} {...pageProps} />
+  return <Component className={firaCode.className} {...pageProps} />
 }
