2d65ab805eb7334c80291b9a53c21ee5a93982dc
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-01-30 17:44:17 -0300

    refactor: :recycle: Alteração do componente de exibição de imagem
    Para o componente Image do next. As imagens dos certificados agora são locais e não de API externa

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -1,24 +1,34 @@
+import Image from 'next/image';
 import React from 'react'
 
 import styles from '../styles/Certificates.module.css'
+import fundamentos from '../public/images/certificates/fundamentos.png';
+import frontEnd from '../public/images/certificates/front-end.png'
+import backEnd from '../public/images/certificates/back-end.png'
+import cienciaDaComputacao from '../public/images/certificates/ciencia-da-computacao.png'
+import certificadoDeConclusao from '../public/images/certificates/certificado-de-conclusao.png'
 
 function Certificates() {
   const images = [
     {
-      img: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/48938492',
+      img: fundamentos,
       url: 'https://www.credential.net/c0f38491-ccd2-4015-abe6-a7d2dc489628#gs.nnnrwb'
     },
     {
-      img: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/53470344',
+      img: frontEnd,
       url: 'https://www.credential.net/ff9c845c-fd09-408b-95cf-14f5ecb4366e#gs.nnnpn8'
     },
     {
-      img: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/60747524',
+      img: backEnd,
       url: 'https://www.credential.net/e65336e4-a1d6-4bb4-ac19-5812ba7a2205#gs.nnkn8y'
     },
     {
-      img: 'https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/66831590',
+      img: cienciaDaComputacao,
       url: 'https://www.credential.net/97400391-3c3a-438f-b9be-2f3b6ef56098#gs.nkf2eh'
+    },
+    {
+      img: certificadoDeConclusao,
+      url: 'https://www.credential.net/c4a084ed-7f15-4a17-97a3-e4063adddfa7'
     }
   ];
 
@@ -30,9 +40,7 @@ function Certificates() {
           images.map((certificate, index) => (
             <div key={index} className={styles['img-card']}>
               <a href={certificate.url} target="_blank" rel="noreferrer">
-                <picture>
-                  <img src={certificate.img} alt="Certificado" />
-                </picture>
+                  <Image src={certificate.img} alt="Certificado" />
               </a>
             </div>
           ))
