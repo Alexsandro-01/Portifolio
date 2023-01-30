b2d77fae8cb98206faf6167a1097516b206f1b64
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-01-30 19:19:21 -0300

    font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import localFont from '@next/font/local'
-const firaCode = localFont({ src: '../public/assets/FiraCode-Medium.woff' })
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={firaCode.className}>
-      <Component {...pageProps} />
-    </main>
-   )
+  return <Component {...pageProps} />
 }
